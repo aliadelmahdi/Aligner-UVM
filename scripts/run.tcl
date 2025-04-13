ba98fd8a64013d5ef7bfee059db2eaf469558336
Ali Adel <aliadelmahdi77@gmail.com>
2025-04-14 01:17:50 +0200

    first commit

---
+++ scripts/run.tcl
@@ -0,0 +1,18 @@
+vlib work
+vlog +incdir+./interface -f "scripts/list.list" -mfcu +cover -covercells
+vsim -voptargs=+acc work.tb_top -cover -classdebug -uvmcontrol=all -fsmdebug 
+log -r /*
+# Code Coverage
+coverage save top.ucdb -onexit
+#/ -du work....
+vcd file waves/waves.vcd
+vcd add -r /* 
+run -all
+vcd flush
+# # Functional Coverage Report
+# coverage report -detail -cvg -directive  \
+#     -output "reports/Functional Coverage Report.txt" \
+#     /../aligner_coverage/*
+quit -sim
+# Save Coverage Report
+vcover report top.ucdb -details -annotate -all -output "reports/Coverage Report - Code, Assertions, and Directives.txt"
